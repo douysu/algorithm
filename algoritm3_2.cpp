1da5bb21bdcf56d96efffe562620ec2200b32905
ModestBean <yindou97@163.com>
2019-06-18 21:47:05 +0800

    algorithm3_2

--- algoritm3_2.cpp
+++ algoritm3_2.cpp
@@ -1,6 +1,21 @@
 #include <stdio.h>
 int main(){
+	const int maxn=100000;
 	int n;
 	scanf("%d",&n);
-	int a[n];
+	int allScore[maxn]={0};
+	int id,score; 
+	for(int i=0;i<n;i++) 
+	{
+		scanf("%d%d",&id,&score);
+		allScore[id-1]+=score;
+	} 
+	int k=-1,max=-1;
+	for(int i=0;i<n;i++){
+		if(allScore[i]>max){
+			max=allScore[i];
+			k=i+1;
+		}
+	}
+	printf("%d %d",k,max);
 }
